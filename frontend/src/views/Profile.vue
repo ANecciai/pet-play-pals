c89d34aceb9c668b2ba06baa527225a5871104a8
Paige Tavormina <ptavormina21@gmail.com>
2021-08-11 11:42:57 -0400

    update

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,8 +1,11 @@
 <template>
-<div>
+<div >
     <h2>WELCOME BACK, {{$store.state.user.username.toUpperCase()}}! </h2>
     <h3> YOUR PETS </h3>
-    <p v-for="pet in $store.state.pets" v-bind:key="pet.id">{{pet.name}}&nbsp;|&nbsp;{{pet.species}}</p>
+   <div v-for="pet in $store.state.pets" v-bind:key="pet.id">
+   <router-link v-bind:to="{name: 'pet-details', params: {petId:pet.petId}}">{{pet.name}}&nbsp;|&nbsp;{{pet.species}}</router-link>
+   <a id="button" href="#" v-on:click="deletePet(pet.petId)">Delete</a>
+   </div>
     <h3> YOUR PLAYDATES </h3>
     <router-link tag = button v-bind:to="{name: 'pet'}"> ADD PET</router-link>
     </div>
@@ -32,6 +35,13 @@ export default {
    //playdates(){
     //   playdate.getPlaydate().then
    //}
+   deletePet(id){
+       petservice.deletePet(id).then(response =>{
+           if (response.status === 200){
+               this.getPet();
+           }
+       })
+   }
   
 
    
