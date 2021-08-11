8ce31bdef985eb58bf52777710aaf9946ec1db6d
Paige Tavormina <ptavormina21@gmail.com>
2021-08-11 13:43:16 -0400

    update

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -2,16 +2,10 @@
 <div >
     <h2>WELCOME BACK, {{$store.state.user.username.toUpperCase()}}! </h2>
     <h3> YOUR PETS </h3>
-<<<<<<< HEAD
    <div v-for="pet in $store.state.pets" v-bind:key="pet.id">
    <router-link v-bind:to="{name: 'pet-details', params: {petId:pet.petId}}">{{pet.name}}&nbsp;|&nbsp;{{pet.species}}</router-link>
    <a id="button" href="#" v-on:click="deletePet(pet.petId)">Delete</a>
    </div>
-=======
-    <p v-for="pet in $store.state.pets" v-bind:key="pet.id">{{pet.name}}&nbsp;|&nbsp;{{pet.species}}</p>
-     <router-link tag = button v-bind:to="{name: 'pet'}"> ADD PET</router-link>
-
->>>>>>> 735c9d21ebd72cc6e5c0b7396521f5d7e040edd9
     <h3> YOUR PLAYDATES </h3>
     <div v-for="playdate in $store.state.playdates" v-bind:key="playdate.playdateId">
         <router-link v-bind:to="{name:'playdate-details', params: {playdateId:playdate.playDateId}}"> {{playdate.playdateDate}} &nbsp;|&nbsp; Time: {{playdate.playdateTime}}  &nbsp;|&nbsp; Host: {{playdate.hostUsername}} &nbsp;|&nbsp; Invitee: {{playdate.invitedUsername}}</router-link>
