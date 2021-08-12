8f6a292a6066484d2f3302cfbc3e1024d4fe69a2
Ashley Necciai <anecciai@gmail.com>
2021-08-12 13:03:05 -0400

    Users added to DB and username on PetDetails

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -23,7 +23,9 @@ CREATE TABLE users (
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO users (username, password_hash, role) VALUES ('test_account','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'ROLE_USER');
+INSERT INTO users (username, password_hash, role) VALUES ('Sara','$2a$10$rVKA6zvhKw2zIN8rr/JkH.1kl4jTlhIefs7wY01ljYuZZmQzjC6xC', 'ROLE_USER');
+INSERT INTO users (username, password_hash, role) VALUES ('John', '$2a$10$rVKA6zvhKw2zIN8rr/JkH.1kl4jTlhIefs7wY01ljYuZZmQzjC6xC', 'ROLE_USER');
+
 CREATE TABLE profile (
     profile_id serial PRIMARY KEY,
     username varchar (50) NOT NULL,
@@ -32,6 +34,10 @@ CREATE TABLE profile (
     zipcode int NOT NULL,
     about_me text,
     FOREIGN KEY (username) REFERENCES users(username));
+
+INSERT INTO profile (username, first_name, last_name, zipcode, about_me) VALUES ('Sara', 'Sara', 'Jane', '15316', 'Hi, my name is Sara! Feel free to make a playdate!');
+INSERT INTO profile (username, first_name, last_name, zipcode, about_me) VALUES ('John', 'John', 'Doe', '15222', 'Help me my house is a zoo');
+
 CREATE TABLE pet (
   pet_id serial PRIMARY KEY,
   username varchar(50),
@@ -45,11 +51,16 @@ CREATE TABLE pet (
   CONSTRAINT FK_pet FOREIGN KEY (username) REFERENCES users(username),
   CONSTRAINT CHK_activity_level CHECK (activity_level IN ('High', 'Medium', 'Low'))
   );
-INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('test_account', 'Spot', 'Dog', 'Dalmatian', 'High', 'Male', '2', 'This is spot');
-CREATE TABLE playdate_status(
-  status_id serial PRIMARY KEY,
-  status_type varchar(50) UNIQUE
-);
+
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('Sara', 'Spot', 'dog', 'Dalmatian', 'High', 'Male', '2', 'This is Spot.');
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('Sara', 'Fluffy', 'cat', 'Maine Coon', 'Medium', 'Female', '5', 'Fluffy lives up to her name! She sleeps hard and plays harder!');
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('John', 'Tweety', 'bird', 'Conure', 'High', 'Female', '1', 'No cats please!');
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('John', 'Bentley', 'dog', 'Mix', 'Medium', 'Male', '2', 'Lab/Beagle Mix');
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('John', 'Peanut', 'dog', 'Mastiff', 'Low', 'Male', '3', 'Gentle giant');
+INSERT INTO pet (username, pet_name, species, breed, activity_level, gender, age, description) VALUES ('John', 'Whiskers', 'cat', 'Orange', 'Low', 'Male', '2', 'He is orange.');
+
+
+
 CREATE TABLE playdate (
   playdate_id serial PRIMARY KEY,
   address varchar(50) NOT NULL,
@@ -62,10 +73,14 @@ CREATE TABLE playdate (
   status_type varchar (50),
   host_username varchar(50) NOT NULL,
   invited_username varchar(50) NOT NULL,
-  FOREIGN KEY (status_type) REFERENCES playdate_status(status_type),
+  CONSTRAINT CHK_status_type CHECK (status_type IN ('Accepted','Declined','Canceled','Pending')),
   FOREIGN KEY (host_username) REFERENCES users(username),
-  FOREIGN KEY (invited_username) REFERENCES users(username)
-  );
+  FOREIGN KEY (invited_username) REFERENCES users(username));
+
+  INSERT INTO playdate (address, city, state, zip_code, playdate_time, playdate_date, playdate_description, status_type, host_username, invited_username)
+  VALUES ('123 Pitt Ave', 'Pittsburgh', 'PA', '15222', '10:00', '10-12-2021', 'I will be the one on the corner with the giant dog!', 'Pending', 'John', 'Sara');
+
+
 CREATE TABLE forum(
   forum_id serial PRIMARY KEY,
   username varchar(50),
@@ -75,8 +90,5 @@ CREATE TABLE forum(
   forum_post text NOT NULL,
   FOREIGN KEY (username) REFERENCES users(username)
   ); 
-INSERT INTO playdate_status (status_type) VALUES ('Accepted');
-INSERT INTO playdate_status (status_type) VALUES ('Pending');
-INSERT INTO playdate_status (status_type) VALUES ('Declined');
-INSERT INTO playdate_status (status_type) VALUES ('Canceled');
+
 COMMIT TRANSACTION;
